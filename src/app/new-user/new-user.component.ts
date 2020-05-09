6f7e14f8edad9c0ad5b11d9c27dcb08f2c51a6f2
Rajdeep5674 <raj.bhadra94@gmail.com>
2020-05-09 13:12:13 +0530

    2 files modified

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {User} from './user';
+
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
